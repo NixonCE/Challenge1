514ffcf77f547231f6c2117e5dc8f05db05c4b3c
Nixon Cespedes <nixoncg27@gmail.com>
2024-04-23 20:05:10 -0600

    tercer commit

--- src/Principal.java
+++ src/Principal.java
@@ -1,6 +1,23 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
 public class Principal {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner lectura = new Scanner(System.in);
         System.out.println("hola mundo");
         System.out.println("prueba");
+        String direccionApi = "https://v6.exchangerate-api.com/v6/50c33799565cdf1cf11364c0/latest/USD";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(direccionApi)).build();
+        HttpResponse<String> response = client.send(request , HttpResponse.BodyHandlers.ofString());
+
+        String data = response.body();
+        System.out.println(data);
+
     }
 }
---
+++ src/monedas.java
@@ -0,0 +1,19 @@
+public class monedas {
+
+
+    public int argentina;
+    public int boliviano;
+    public int brasileño;
+    public int chileno;
+    public int colombiano;
+    public int estadounidense;
+
+    public monedas(int ARS, int BOB, int BRL, int CLP, int COP, int USD) {
+        this.argentina = ARS;
+        this.boliviano = BOB;
+        this.brasileño = BRL;
+        this.chileno = CLP;
+        this.colombiano = COP;
+        this.estadounidense = USD;
+    }
+}
